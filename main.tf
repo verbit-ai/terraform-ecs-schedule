cfa908dbba80a30f97387e4ae0856f3e8d387045
Mikhail Vasilenko <mikhailv@verbit.ai>
2021-01-15 19:09:46 +0200

    update to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 resource "aws_cloudwatch_event_rule" "scheduled_task" {
-  name                = "${var.target_id}"
+  name                = var.target_id
   description         = "Run ecs task at a scheduled time"
-  schedule_expression = "${var.schedule_expression}"
+  schedule_expression = var.schedule_expression
 }
 
 resource "aws_cloudwatch_event_target" "scheduled_task" {
-  target_id = "${var.target_id}"
-  rule      = "${aws_cloudwatch_event_rule.scheduled_task.name}"
-  arn       = "${var.cluster_arn}"
-  role_arn  = "${var.role_arn}"
+  target_id = var.target_id
+  rule      = aws_cloudwatch_event_rule.scheduled_task.name
+  arn       = var.cluster_arn
+  role_arn  = var.role_arn
 
   ecs_target {
-    task_count          = "${var.task_count}"
-    task_definition_arn = "${var.task_def_arn}"
+    task_count          = var.task_count
+    task_definition_arn = var.task_def_arn
   }
 
   input = <<DOC
@@ -25,4 +25,6 @@ resource "aws_cloudwatch_event_target" "scheduled_task" {
   ]
 }
 DOC
+
 }
+
